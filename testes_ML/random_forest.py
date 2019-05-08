5c9ea0b5af21527a7e10f37a662340b0cd37a296
danieltatsch <danielt.tatsch@gmail.com>
2019-05-07 23:00:44 -0300

    Add loop to verify best value of n_estimators

--- testes_ML/random_forest.py
+++ testes_ML/random_forest.py
@@ -6,11 +6,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics 		 import confusion_matrix, classification_report, accuracy_score
 from sklearn.preprocessing 	 import StandardScaler
 from sklearn.ensemble 		 import RandomForestClassifier
+from sklearn 				 import metrics
 
 plt.style.use('ggplot')
 
 #Carrega o dataset
-df = pd.read_csv('datasets/cancer.csv')
+df = pd.read_csv('datasets/bill_authentication.csv')
 
 #Print das 5 primeiras linhas
 print("----------------------------")
@@ -26,8 +27,8 @@ print("(linhas, colunas): " + str(df.shape))
 print("----------------------------")
 
 #Criando arrays separados para os dados e os resultados
-X = df.drop('diagnosis',axis=1).values #dados
-y = df['diagnosis'].values #resultados
+X = df.drop('Class',axis=1).values #dados
+y = df['Class'].values #resultados
 
 # print(X)
 # print(y)
@@ -38,16 +39,46 @@ y = df['diagnosis'].values #resultados
 #stratify 	  -> separar por y (resultados)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0, stratify=y)
 
+'''
+#Verificando precisao do Random Forest para diferentes quantidades de arvores
+trees = np.arange(1,10)
+train_accuracy =np.empty(len(trees))
+test_accuracy = np.empty(len(trees))
+
+for i,N in enumerate(trees):
+    #Configura o classificador com k vizinhos
+	random_forest = RandomForestClassifier(n_estimators=N)  
+    
+    #divide o modelo
+	random_forest.fit(X_train, y_train)
+    
+    #Calcula a precisao dos dados de treinamento
+	train_accuracy[i] = random_forest.score(X_train, y_train)
+    
+    #Calcula a precisao dos dados de teste
+	test_accuracy[i] = random_forest.score(X_test, y_test)
+
+plt.title('Random Forest variando num de arvores para analise')
+plt.plot(trees, test_accuracy, label='Precisao no teste')
+plt.plot(trees, train_accuracy, label='Precisao no treinamento')
+plt.legend()
+plt.xlabel('Numero de arvores')
+plt.ylabel('Precisao')
+plt.show()
+
+'''
+
 #n_estimators -> numero de arvores que serao criadas na analise
 #random_state -> semente para dividir os dados randomicamente
-random_forest = RandomForestClassifier(n_estimators=20, random_state=0)  
+random_forest = RandomForestClassifier(n_estimators=6, random_state=0)  
 
 #Ajusta os modelos
 random_forest.fit(X_train, y_train)  
 
-print("Precisao: " + str(random_forest.score(X_test,y_test)))
-
 y_pred = random_forest.predict(X_test)
+
+print("Precisao: " + str(random_forest.score(X_test,y_test)))
+print("Precisao: " + str(metrics.accuracy_score(y_test,y_pred)))
 
 print("----------------------------")
 print("Matriz de confusao")
